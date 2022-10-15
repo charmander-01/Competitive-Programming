b59d6279ba650118bb113acfc97abb00e5727b7c
Luís Miguel <92678690+charmander-01@users.noreply.github.com>
2022-10-15 10:06:45 -0300

    Add files via upload

---
+++ Maratona (Regional) 2022/f.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, c, att, cont;
+int aux = 0;
+string s, vitoria;
+
+vector <string> vet;
+vector < pair <string, int> > palavra;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> c;
+
+    for(int i = 0; i < n; i++){
+
+        cin >> s;
+        
+        for(int i = 0; i < c; i++){
+            if(s[i] == '*'){
+                att = i;
+                break;
+            }
+        }
+
+        for(int i = 0; i < 26; i++){
+            s[att] = 'a' + i;
+            vet.push_back(s);
+            //cout << s << "\n";
+ 
+        }
+
+    }
+
+    palavra.resize(vet.size());
+
+    sort(vet.begin(), vet.end());
+
+    // cout << "**********";
+
+    for(int i = 0; i < vet.size(); i++){
+        int j;
+        cont = 1;
+        j = i;
+        if(j!=vet.size())
+        {
+            if(vet[j] == vet[j+1])
+            {
+                while(vet[j]==vet[j+1])
+                {
+                    cont++;
+                    j++;               
+                }
+
+                i = j;
+            }
+        }
+        // cout << "**********";
+        palavra[i].first = vet[i];
+        palavra[i].second = cont;
+        if(cont>aux)
+        {
+            vitoria = palavra[i].first;
+            aux = cont;
+        }
+        // cout << "**********";
+    }
+
+    cout << vitoria << " " << aux << "\n";
+
+    return 0;
+}
